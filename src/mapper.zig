14d69d3bd7eb4faf562a848d5153cc79a41f0ecd
Matt Renfro <evalandprosper@gmail.com>
2026-01-10 00:19:10 -0600

    WIP on basic cart reading and mapper interface

---
+++ src/mapper.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+const Cartridge = @import("cartridge.zig").Cartridge;
+const NesHeader = @import("cartridge.zig").NesHeader;
+
+// Mapper Interface
+const Mapper = struct {
+    impl: *anyopaque,
+    vtable: *const VTable,
+
+    const VTable = struct {
+        read: *const fn (self: *anyopaque, address: u16) u8,
+        write: *const fn (self: *anyopaque, address: u16, value: u8) void,
+        // ppuRead: *const fn(self: *Mapper, address: u16) u8,
+        // ppuWrite: *const fn(self: *Mapper, address: u16, value: u8) void,
+        // scanline: *const fn(self: *Mapper, ) void,
+        deinit: *const fn (self: *anyopaque, allocator: std.mem.Allocator) void,
+    };
+
+    pub fn init(impl: anytype) Mapper {
+        const T = std.meta.Child(@TypeOf(impl));
+        return .{
+            .impl = impl,
+            .vtable = &.{
+                .read = @ptrCast(&@field(T, "read")),
+                .write = @ptrCast(&@field(T, "write")),
+                .deinit = @ptrCast(&@field(T, "deinit")),
+            }
+        };
+    }
+
+    pub fn read(self: *Mapper, address: u16) u8 {
+        return self.vtable.read(self.impl, address);
+    }
+
+    pub fn write(self: *Mapper, address: u16, value: u8) void {
+        return self.vtable.write(self.impl, address, value);
+    }
+
+    pub fn deinit(self: *Mapper, allocator: std.mem.Allocator) void {
+        return self.vtable.deinit(self.impl, allocator);
+    }
+};
+
+const Mapper0 = struct {
+    cart: *Cartridge,
+
+    // pub fn init(cart: *Cartridge) Mapper0 {
+    //     return .{ .cart = cart };
+    // }
+
+    pub fn init(self: *Mapper0, allocator: std.mem.Allocator, cart: *Cartridge) !void {
+        _ = allocator;
+        self.cart = cart;
+    }
+
+    fn read(self: *Mapper0, address: u16) u8 {
+        // _ = self;
+        _ = address;
+        std.debug.print("Mapper0 read\n", .{});
+        std.debug.print("self ptr: {*}\n", .{self});
+        std.debug.print("self.cart ptr: {*}\n", .{self.cart});
+
+
+        return 0;
+    }
+
+    fn write(self: *Mapper0, address: u16, value: u8) void {
+        _ = self;
+        _ = address;
+        _ = value;
+        std.debug.print("Mapper0 write\n", .{});
+    }
+
+    fn deinit(self: *Mapper0, allocator: std.mem.Allocator) void {
+        allocator.destroy(self);
+    }
+};
+
+pub fn mapperFactory(allocator: std.mem.Allocator, cart: *Cartridge) !Mapper {
+    const mapper_num = cart.header.mapper();
+    return switch (mapper_num) {
+        0 => {
+            const m = try allocator.create(Mapper0);
+            m.* = .{ .cart = cart };
+            try m.init(allocator, cart);
+            return Mapper.init(m);
+        },
+        else => @panic("Unsupported Mapper!")
+    };
+}
+
+test "Mapper 0" {
+    const alloc = std.testing.allocator;
+
+    var cart = Cartridge{
+        .header = std.mem.zeroes(NesHeader),
+        .prgROM = null,
+        .chrROM = null
+    };
+    std.debug.print("cart ptr: {*}\n", .{&cart});
+
+    var mapper = Mapper0{ .cart = &cart };
+    try mapper.init(alloc, &cart);
+    _ = mapper.read(0);
+
+    // test interface
+    var mapper_interface = Mapper.init(&mapper);
+    _ = mapper_interface.read(0);
+
+    // factory test
+    var fac_interface = try mapperFactory(alloc, &cart);
+    defer fac_interface.deinit(alloc);
+    _ = fac_interface.read(0);
+
+    _ = Mapper.init(&cart);
+}
